5c5a49f08adf45bfce005dde9da74e851c8b9a48
Simargl05 <a373796@uach.mx>
2026-05-25 00:23:22 -0600

    c mamo

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROJECT_ID.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROJECT_ID.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyAtsWj7I-HZCZoLyXAmqO0RBz2pUDXaYiA",
+  authDomain: "nurse-flow-d0253.firebaseapp.com",
+  projectId: "nurse-flow-d0253",
+  storageBucket: "nurse-flow-d0253.firebasestorage.app",
+  messagingSenderId: "1060790212453",
+  appId: "1:1060790212453:web:9c309e24915785cdd6b38c",
+  measurementId: "G-KG1X5NLQYS"
 };
 
 const app = initializeApp(firebaseConfig);
